dc956098211f0a13d62dbd0afbcd0bb70f111fcf
shmuel asherov <shmuelas7@gmail.com>
2022-08-19 01:09:03 +0300

    creat folder

--- Routes/fileRouter.js
+++ Routes/fileRouter.js
@@ -7,11 +7,23 @@ const fileLogic = require("../BL/fileLogic");
 router.get("/root/:path?", async (req, res) => {
   {
     try {
-      const listfiles = fileLogic.get_fils(req.params.path);
+      console.log(req.query.path);
+      const listfiles = await fileLogic.get_fils(req.query.path);
+
       res.send(listfiles);
-    } catch {
-      res.send("errrrrrr");
+    } catch (err) {
+      res.send(err);
     }
+  }
+});
+
+router.post("/createFolder", function (req, res) {
+  try {
+    console.log(req.body.name);
+    const create = fileLogic.createFolder(req.body.name, req.body.path);
+    return create;
+  } catch (err) {
+    return err;
   }
 });
 
